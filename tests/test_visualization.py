31fc3671ca70dbe91a2778b9430441138af937a3
TristanHUC <97129651+TristanHUC@users.noreply.github.com>
2024-03-04 20:12:24 +0100

    Update test_visualization.py

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,14 +1,14 @@
 # test_visualization.py
 import unittest
 import os
-from work.utils import count_figure
+from ..work.utils import count_figure
 
 class test_output(unittest.TestCase):
 
     def test_output(self):
 
         filename = 'exemple_pdf1.grobid.tei.xml'
-        directory = "./tests/input_files"
+        directory = "../tests/input_files"
 
         file_path = os.path.join(directory, filename)
 
